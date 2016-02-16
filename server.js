04d9864e242e0ca302982462b2a5359384a62154
AmitaPatel3 <amitapatel3@hotmail.com>
2016-02-16 14:06:44 -0700

    created post.bear

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/animals');
 
+var Bear = require('./models/bear');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -12,9 +14,44 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-	res.json({title: 'hooray! welcome to our api!'});
+router.use(function(req, res, next) {				//this is the same as app.post in 'bear files'
+	console.log('something is happening!');
+	next();
 });
+
+
+router.get('/', function(req, res) {
+	res.json({title: 'hooray! it worked!'});
+});
+
+
+router.route('/bears')
+	.post(function(req,res) {
+
+		var bear = new Bear();
+
+		bear.name = req.body.name;
+		bear.age = req.body.age;
+		bear.gender = req.body.gender;
+
+
+		bear.save(function(err,bear) {
+			if(err){
+				console.log(err)
+			} else {
+				res.json(bear)
+
+			}
+
+		})
+	})
+		
+
+	.get(function(req,res) {
+		res.json({title: "I am trying to GET"})
+	});
+
+
 
 app.use('/api', router);  //this states that fill in api after //
 
